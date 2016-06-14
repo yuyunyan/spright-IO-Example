c8bcc1d1c42a1302b9e7ce133c3d93a40c158317
Julia Yu <juliayu9@gmail.com>
2016-06-13 17:02:04 -0700

    Angular Config

---
+++ dealerInventory/ngConfig/dealerInventory.config.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+        .config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+
+            $routeProvider.when('/list', {
+                templateUrl: '/Scripts/sabio/application/dealerInventory/templates/list.html',
+                controller: 'listController',
+                controllerAs: 'list'
+            }).when('/edit/:recordID', {
+                templateUrl: '/Scripts/sabio/application/dealerInventory/templates/edit.html',
+                controller: 'vehicleEditController',
+                controllerAs: 'edit'
+            }).when('/create', { //edit and create share the same view and controller 
+                templateUrl: '/Scripts/sabio/application/dealerInventory/templates/edit.html',
+                controller: 'vehicleEditController',
+                controllerAs: 'edit'
+            }).when('/media/:recordID', { //edit and create share the same view and controller 
+                templateUrl: '/Scripts/sabio/application/dealerInventory/templates/media.html',
+                controller: 'vehicleMediaController',
+                controllerAs: 'ava'
+            }).when('/video/:recordID', { //edit and create share the same view and controller 
+                templateUrl: '/Scripts/sabio/application/dealerInventory/templates/video.html',
+                controller: 'vehicleVideoController',
+                controllerAs: 'vvc'
+            });
+
+            $locationProvider.html5Mode(false);
+
+        }]);
+
+})();
+
