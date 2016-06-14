efcf28769062a9e4ff37de1d7aa1e493ec22312e
Julia Yu <juliayu9@gmail.com>
2016-06-13 17:52:00 -0700

    Dealer Record Service

---
+++ dealerInventory/middle-tier/Service/Interface/IDealerRecordService.cs
@@ -0,0 +1,24 @@
+﻿using Sabio.Web.Domain.EVA;
+using Sabio.Web.Models.Requests;
+using Sabio.Web.Models.Requests.EVA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sabio.Web.Services.Interfaces
+{
+    public interface IDealerRecordService
+    {
+        int InsertRecord(DealerRecordAddRequest model);
+        List<EVA_Record> ListVehicleRecordByDealerID(int DealerID, PaginateListRequestModel model);
+        int GetVehicleRecord_Count(int DealerID);
+        int InsertDealerRecordMedia(DealerRecordMediaRequest model);
+        List<Domain.EVA.DealerRecordMedia> GetMediasByRecordId(int recordID);
+        void setMainPhoto(RecordMediaSetMainPhoto model);
+        void DeleteByID(int DealerID, int RecordID);
+        void DeleteMediaByID(DealerRecordMediaRequest model);
+        void DeleteVideoByID(DealerRecordMediaRequest model);
+
+    }
+}
