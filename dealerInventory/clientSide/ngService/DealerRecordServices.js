7c7cedfe1180dba18c15fa7c4347a0aeb97190b8
Julia Yu <juliayu9@gmail.com>
2016-06-14 23:04:43 -0700

    angularService

---
+++ dealerInventory/clientSide/ngService/DealerRecordServices.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+         .factory('$dealerRecordService', DealerRecordService);
+
+    DealerRecordService.$inject = ['$baseService', '$sabio'];    //  $sabio is a reference to sabio.page object which is created in sabio.js
+
+    function DealerRecordService($baseService, $sabio) {
+
+        var aSabioServiceObject = sabio.services.dealerRecord;
+
+        var newService = $baseService.merge(true, {}, aSabioServiceObject, $baseService);
+
+        return newService;
+    }
+
+})();
