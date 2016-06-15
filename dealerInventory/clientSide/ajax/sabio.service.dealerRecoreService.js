0d2df1c308cfc5c2084b39081012c3c59fa758b7
Julia Yu <juliayu9@gmail.com>
2016-06-14 22:53:23 -0700

    ajax calls

---
+++ dealerInventory/clientSide/ajax/sabio.service.dealerRecoreService.js
@@ -0,0 +1,98 @@
+﻿if (!sabio.services.dealerRecord)
+    sabio.services.dealerRecord = {}
+
+// create vehicle
+sabio.services.dealerRecord.addRecord = function (payload, onSuccess, onError) {
+    $.ajax({
+        type: 'POST',
+        data: payload,
+        dataType: "json",
+        url: '/api/DealerRecord',
+        success: onSuccess,
+        error: onError
+    });
+};
+
+sabio.services.dealerRecord.listRecords = function (payload,onSuccess, onError) {
+    $.ajax({
+        type: 'GET',
+        data: payload,
+        dataType: "json",
+        url: '/api/DealerRecord',
+        success: onSuccess,
+        error: onError
+    });
+};
+
+sabio.services.dealerRecord.insertPictures = function (payload, onSuccess, onError) {
+    $.ajax({
+        type: 'POST',
+        data: payload,
+        dataType: "json",
+        url: '/api/DealerRecord/insertPicture',
+        success: onSuccess,
+        error: onError
+    });
+};
+
+
+sabio.services.dealerRecord.getMediaByRecordID= function (recordID, onSuccess, onError) {
+    $.ajax({
+        type: 'GET',
+        data: recordID,
+        dataType: "json",
+        url: '/api/DealerRecord/getPicture/' + recordID,
+        success: onSuccess,
+        error: onError
+    });
+};
+
+sabio.services.dealerRecord.setMainPicture = function (payload, onSuccess, onError) {
+    $.ajax({
+        type: 'POST',
+        data: payload,
+        dataType: "json",
+        url: '/api/DealerRecord/SetMainPhoto',
+        success: onSuccess,
+        error: onError
+    });
+};
+
+sabio.services.dealerRecord.deleteRecordByDealerIdRecordId = function (recordID, onSuccess, onError) {
+    $.ajax({
+        type: 'DELETE',
+        data: recordID,
+        dataType: "json",
+        url: '/api/DealerRecord/deleteRecord/' + recordID,
+        success: onSuccess,
+        error: onError
+    });
+
+};
+// must use request model for the payload 
+sabio.services.dealerRecord.deleteMediaByMediaIdRecordId = function (payload, onSuccess, onError) {
+    $.ajax({
+        type: 'DELETE',
+        data: payload,
+        dataType: "json",
+        url: '/api/DealerRecord/deleteMedia',
+        success: onSuccess,
+        error: onError
+    });
+}
+
+    
+    sabio.services.dealerRecord.deleteVideoByMediaIdRecordId = function (payload, onSuccess, onError) {
+        $.ajax({
+            type: 'DELETE',
+            data: payload,
+            dataType: "json",
+            url: '/api/DealerRecord/deleteVideo',
+            success: onSuccess,
+            error: onError
+        });
+
+};
+
+
+
